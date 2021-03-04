ba8560f144e0530c78edc51a86ec617fa3dbec3a
Marilyn Nguyen <mariln1@uci.edu>
2021-03-04 15:53:17 -0800

    Signup button, added TODOs

--- app/src/main/java/com/example/omnidrive/ui/signup/SignupActivity.java
+++ app/src/main/java/com/example/omnidrive/ui/signup/SignupActivity.java
@@ -7,14 +7,23 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.omnidrive.R;
+import com.example.omnidrive.ui.login.LoginActivity;
 
 public class SignupActivity extends Activity {
-    Button signUpActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        final Button signup = findViewById(R.id.signupBtn);
+        signup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO: add user info to database
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
