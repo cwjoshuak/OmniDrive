9170ab3db24bad4e53831efa1b38352749f11f62
Ashhad <shah.ashhad@gmail.com>
2021-03-04 13:40:21 -0800

    added welcome page plus button activities

--- app/src/main/java/com/example/omnidrive/ui/signup/SignupActivity.java
+++ app/src/main/java/com/example/omnidrive/ui/signup/SignupActivity.java
@@ -1,14 +1,20 @@
 package com.example.omnidrive.ui.signup;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.omnidrive.R;
 
 public class SignupActivity extends Activity {
+    Button signUpActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
     }
 }
